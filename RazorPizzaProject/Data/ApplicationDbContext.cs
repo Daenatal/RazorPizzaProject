90e108c0a606b643abc792a86fba1613af8a9b00
Jesus <daenatal@yahoo.com>
2022-11-22 20:32:27 -0600

    Added DbContext to connect to local SQL DB and create Db for PizzaApp and create a table to store orders.

---
+++ RazorPizzaProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorPizzaProject.Models;
+
+namespace RazorPizzaProject.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<PizzaOrder> PizzaOrders { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+    }
+}
